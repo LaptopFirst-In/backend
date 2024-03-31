d7a5bb45d20c315018060226310800db094ce6f4
Manvendra Singh <ms1577239@gmail.com>
2024-03-31 19:09:15 +0530

    Manvendra's first commit with OTP sent functionality

--- src/routers/aadhaar.ts
+++ src/routers/aadhaar.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {aadhaarData} from '../controllers/aadhaar'
+import {aadhaarData} from '../controllers/aadhaarOtpSent'
 
 export default (router: express.Router) => {
   router.post("/aadhaar", aadhaarData);
