61a6e536ae236c0283bbcf9fd78c2f26d76d8f1d
Abhijat-Abhijat <abhijat789@gmail.com>
2024-04-03 12:04:17 +0530

    fix(): IP issue (integrated signature)

--- src/controllers/aadhaarOtpSent.ts
+++ src/controllers/aadhaarOtpSent.ts
@@ -1,27 +1,27 @@
 import express from "express";
 import axios from "axios";
+import {generateSignature} from '../helpers'
 export const registerAadhaar = async (
   req: express.Request,
   res: express.Response
 ) => {
   try {
+    const signature = generateSignature();
     const options = {
-      method: "POST",
-      url: "https://sandbox.cashfree.com/verification/offline-aadhaar/otp",
+      method: 'POST',
+      url: 'https://sandbox.cashfree.com/verification/offline-aadhaar/otp',
       headers: {
-        accept: "application/json",
-        "content-type": "application/json",
-        "x-client-id": "CF10117541CO4NB3LM103MD67HR8J0",
-        "x-client-secret":
-          "cfsk_ma_test_16eb5333e806bd98f531dae85fdc4365_4610ceba",
+        accept: 'application/json',
+        'content-type': 'application/json',
+        'x-client-id': 'CF10117541CO4NB3LM103MD67HR8J0',
+        'x-client-secret': 'cfsk_ma_test_16eb5333e806bd98f531dae85fdc4365_4610ceba',
+        "x-cf-signature": signature,
       },
       data: { aadhaar_number: req.headers.aadhaar_number },
     };
-    axios.request(options).then(function (response) {
-      console.log(response.data);
-      
-      return res.status(200).send({ data:response.data });
-    });
+    const response = await axios.request(options);
+    console.log(response.data);
+    return res.status(200).send({ data: response.data });
   } catch (error) {
     console.log(error);
     return res
--- src/controllers/aadhaarOtpVerify.ts
+++ src/controllers/aadhaarOtpVerify.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import axios from "axios";
+import {generateSignature} from '../helpers'
 export const verifyAaadhar = async (
   req: express.Request,
   res: express.Response
 ) => {
   try {
+    const signature = generateSignature();
     const options = {
         method: 'POST',
         url: 'https://sandbox.cashfree.com/verification/offline-aadhaar/verify',
@@ -12,13 +14,13 @@ export const verifyAaadhar = async (
           accept: 'application/json',
           'content-type': 'application/json',
           'x-client-id': 'CF10117541CO4NB3LM103MD67HR8J0',
-          'x-client-secret': 'cfsk_ma_test_16eb5333e806bd98f531dae85fdc4365_4610ceba'
+          'x-client-secret': 'cfsk_ma_test_16eb5333e806bd98f531dae85fdc4365_4610ceba',
+          "x-cf-signature": signature,
         },
         data: {otp: req.headers.otp, ref_id: req.headers.ref_id}
       };
-      axios.request(options).then(function (response) {
+      const response = await axios.request(options);
         return res.status(200).send({ data: response.data });
-      });
   } catch (error) {
     console.log(error);
     return res
