b5399f141b676eca45711639538a2abcffac14f3
Abhijat-Abhijat <abhijat789@gmail.com>
2024-04-06 15:30:01 +0530

    Final Push

--- src/routers/aadhaar.ts
+++ src/routers/aadhaar.ts
@@ -5,4 +5,7 @@ import { verifyAaadhar } from "../controllers/aadhaarOtpVerify";
 export default (router: express.Router) => {
   router.post("/registerAadhaar", registerAadhaar);
   router.post("/verifyAadhaar", verifyAaadhar);
+  router.get("/", (req, res) => {
+    res.send("Welcome to the homepage");
+  });
 };
