8f395c39d98a1f655c3b92bf97b9b751013aec66
Abhijat-Abhijat <abhijat789@gmail.com>
2024-04-06 16:08:04 +0530

    fix

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ const config= {
       cashfreeurl: 'https://sandbox.cashfree.com/verification/offline-aadhaar',
       cashfreeCliendId : '',
       cashfreeClientSecret:'',
-      cashfreepublickey:   `-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsgIBaO+D/VCpQx2Dbsn2JVZjH8tAfG+tFH3d7wQssteQ6A0664ujXUUhR9ovIaess5x68z2JwaVmYLJ9ZoFolUFmKltISNhbsOW8iK+i5kgTjGpjMfeMIuQlPq71j+ulC9IGrtr3UyBjMxmB5eLun3vZowFwsqwa3YLawBvC8H02ABWTYXt2NzYgw1q2jpWKR7XS/8sD0EcMPWbaejxOImKBKc2y8RFBEvBXqUnnUYKk25ey022w9apnslF3Nv+4+mjwrDqt2fF5S3UKAbgN07SMNqwwq0cQlnDOlmTNGebsfQrEN585UcN1JjOzYu5f6a32vrPX/Mbf23SyeIFsOwIDAQAB\n-----END PUBLIC KEY-----`,
+      cashfreepublickey:  '',
 
     },
   
